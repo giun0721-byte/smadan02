6d9d31d46f8ca5c9a0331fc6c576cf214ca712a0
Giun Tanaka <giun0721@gmail.com>
2025-11-17 11:54:41 +0900

    Web更新: service worker更新

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -56,7 +56,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ff6867e01d8c2aaac8a0d9e062377546
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "10c3ca7eac765eaac44a4fb52f63c3d5",
+"flutter_bootstrap.js": "2121aacaf2755aaca24e3624c6fa130b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
